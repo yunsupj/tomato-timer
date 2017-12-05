b16f14027fc4b105a21104c8b26a01dbab499ea0
yunsupj <yunsup.j@gmail.com>
2017-12-04 16:00:13 -0800

    final_ver

---
+++ components/Timer/presenter.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import Button from "../Button";
+
+function formatTime(time) {
+  let minutes = Math.floor(time/60);
+  let seconds = time%60
+  return `${minutes<10 ? `0${minutes}` : minutes}:${seconds<10 ? `0${seconds}` : seconds}`
+}
+
+class Timer extends Component {
+componentWillReceiveProps(nextProps) {
+  const currentProps = this.props;
+  //console.log(`Current Props: ${currentProps.isPlaying} and New Props: ${nextProps.isPlaying}`)
+  if (!currentProps.isPlaying && nextProps.isPlaying) {
+    const timerInterval = setInterval(() => {
+      currentProps.addSecond()
+    }, 1000);
+
+    this.setState({
+      timerInterval
+    });
+
+  } else if (currentProps.isPlaying && !nextProps.isPlaying) {
+    clearInterval(this.state.timerInterval);
+  }
+}
+
+  render() {
+    //console.log(this.props);
+    const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer, addSecond } = this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.upper}>
+          <Text style={styles.time}>{formatTime(timerDuration - elapsedTime)}</Text>
+        </View>
+        <View style={styles.lower}>
+          {!isPlaying && (
+            <Button iconName={"play-circle"} onPress={startTimer} />
+          )}
+          {isPlaying && (
+            <Button iconName={"stop-circle"} onPress={restartTimer} />
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#CE0B24"
+  },
+
+  upper: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  lower: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  time: {
+    color: "white",
+    fontSize: 120,
+    fontWeight: "100"
+  }
+});
+
+export default Timer;
